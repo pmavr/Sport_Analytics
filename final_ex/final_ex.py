9aedefe0812a4e31e1d474d2f0eeb7adf2cb6707
panagiotis <p.mavrogiannis@outlook.com>
2020-06-13 02:03:11 +0300

    Adapting team predictor

--- final_ex/final_ex.py
+++ final_ex/final_ex.py
@@ -13,6 +13,17 @@ def show_image(img, msg=''):
             break
     cv2.destroyWindow(msg)
 
+
+def show_images(images: list) -> None:
+    n: int = len(images)
+    f = plt.figure()
+    for i in range(n):
+        # Debug, plot figure
+        f.add_subplot(1, n, i + 1)
+        plt.imshow(images[i])
+    plt.show(block=True)
+
+
 def plot_clusters(clrs, labels, n_clusters=2):
     print('[INFO] Plot dominant object colors')
     colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 125, 125), (125, 0, 125)]
@@ -58,17 +69,9 @@ def extract_objects(image, layer_outputs):
 
 
 # input_file = "cutvideo.mp4"
-# input_file = "chelsea_manchester.mp4"
-# input_file = "clips/olympiakos_panaitwlikos.mp4"
-# team1_mask = ([105, 150, 0], [135, 255, 255])
-# team2_mask = ([0, 50, 50], [10, 255, 255])
+input_file = "../clips/chelsea_manchester.mp4"
+# input_file = "../clips/aris_aek.mp4"
 
-
-input_file = "../clips/aris_aek.mp4"
-team1_mask = ([25, 125, 125], [35, 255, 255])
-team2_mask = ([90, 50, 0], [135, 255, 255])
-
-output_file = "cutvideo_out.avi"
 labels_file = "../yolo_files/yolov3.txt"
 config_file = "../yolo_files/yolov3.cfg"
 weights_file = "../yolo_files/yolov3.weights"
@@ -93,7 +96,9 @@ layer_names = [layer_names[i[0] - 1] for i in yolo.getUnconnectedOutLayers()]
 
 vs = cv2.VideoCapture(input_file)
 imgs = []
-
+tmp = [0, 7, 8, 25]
+keep=[]
+h = 0
 for j in range(2):
     success, frame = vs.read()
 
@@ -102,20 +107,26 @@ for j in range(2):
     output = yolo.forward(layer_names)
 
     box_list, _, _ = extract_objects(frame, output)
-    for b in box_list:
-        imgs.append(frame[b[1]:b[1] + b[3], b[0]:b[0] + b[2]])
 
-h = 0
-for i in imgs:
-    cv2.imwrite('../tmp/{}.jpg'.format(h), i)
-    h += 1
+    for b in box_list:
+        if not(h in tmp):
+            imgs.append(frame[b[1]:b[1] + b[3], b[0]:b[0] + b[2]])
+            keep.append(h)
+        h += 1
+
+
+for i, idxs in zip(imgs,keep):
+    cv2.imwrite('../tmp/{}.jpg'.format(idxs), i)
+
+# show_images(imgs)
 
 team_predictor, team_colors = cc.train_clustering(imgs, n_clusters=2)
 
-# plot_clusters(team_colors, team_predictor.labels_, n_clusters=2)
+plot_clusters(team_colors, team_predictor.labels_, n_clusters=2)
 
-something = cc.predict_team(i, team_predictor, n_clusters=2)
+something = cc.predict_team(imgs[0], team_predictor, n_clusters=2)
 print('Labels: {}'.format(team_predictor.labels_))
+# print('TrLabls:{}'.format([1,1,0,1,2,1,0,2,1,1,1,1,0,0,1,2,1,0,1,2]))
 print('Predicted label: {}'.format(something))
 success, frame = vs.read()
 
