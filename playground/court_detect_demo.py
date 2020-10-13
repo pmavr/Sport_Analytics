1cd7eea67e9caa02457415f53cd1cfa1e503c004
panagiotis <p.mavrogiannis@outlook.com>
2020-10-13 18:03:44 +0300

    Upgrading court and line detection method - improved white line pixel detection

--- playground/court_detect_demo.py
+++ playground/court_detect_demo.py
@@ -7,28 +7,27 @@ from auxiliary import aux
 def detect_court(image):
     lower_color = np.array([35, 100, 60])
     upper_color = np.array([75, 255, 200])
-
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_color, upper_color)
     mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel=np.ones((20, 20), np.uint8))
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel=np.ones((20, 20), np.uint8))
-
     img = cv2.bitwise_and(image, image, mask=mask)
     return img
+
 
 def detect_white_line_pixels(image):
-    lower_color = np.array([35, 0, 60])
-    upper_color = np.array([75, 250, 255])
+    lower_color = np.array([35, 0, 145])
+    upper_color = np.array([75, 140, 255])
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_color, upper_color)
-    mask = aux.remove_white_dots(mask, iterations=2)
+    # mask = cv2.morphologyEx(mask, cv2.MORPH_TOPHAT, kernel=np.ones((2, 2), np.uint8))
     img = cv2.bitwise_and(image, image, mask=mask)
     return img
 
 
-input_file = "../clips/belgium_japan.mp4"
-# input_file = "../clips/aris_aek.mp4"
-input_file = "../clips/chelsea_manchester.mp4"
+# input_file = "../clips/belgium_japan.mp4"
+input_file = "../clips/aris_aek.mp4"
+# input_file = "../clips/chelsea_pmanchester.mp4"
 vs = cv2.VideoCapture(input_file)
 success, frame = vs.read()
 
@@ -37,9 +36,7 @@ while success:
 
     img_c = detect_court(img)
 
-    # img_c = detect_white_line_pixels(img_c)
-
-    # cv2.imshow('Display lines', img_c)
+    img_c = detect_white_line_pixels(img_c)
 
     # show dual images
     concat = np.concatenate((img,img_c), axis=1)
