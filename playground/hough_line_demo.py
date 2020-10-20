17a3faacca3e5ee130e3d6c6e8d11bc43a43687f
panagiotis <p.mavrogiannis@outlook.com>
2020-10-20 16:32:05 +0300

    Hough line refinement

--- playground/hough_line_demo.py
+++ playground/hough_line_demo.py
@@ -1,54 +1,27 @@
-
 import cv2
 import numpy as np
+import math
 from auxiliary import HoughLines as hl, aux
 
-def get_line_endpoints(rho, theta):
-    line_length = 2200
-    a = np.cos(theta)
-    b = np.sin(theta)
-    x0 = a * rho
-    y0 = b * rho
-    x1 = int(x0 + line_length * (-b))
-    y1 = int(y0 + line_length * (a))
-    x2 = int(x0 - line_length * (-b))
-    y2 = int(y0 - line_length * (a))
-    return (x1, y1), (x2, y2)
 
-def draw_line(img, rho, theta):
-    (x1, y1), (x2, y2) = get_line_endpoints(rho, theta)
-    cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 1)
-    return img
+def get_angle(point1, point2):
+    """ Calculates the angle of a corner [0,0] - point1 - point2"""
+    cv2.norm([0,0], point1)
 
-def drawhoughLinesOnImage(image, hl_img, houghLines):
-    for (r, th) in houghLines:
-        hl_img = draw_line(image, r, th)
-    return hl_img
+    delta_x = point2[0] - point1[0]
+    delta_y = point1[1] - point2[1]
+    theta_radians = math.atan2(delta_y, delta_x)
+    return theta_radians * 180. / np.pi
 
-def houghLines(image, threshold=95):
-    houghLines = cv2.HoughLines(image,
-                                rho=1,                  # Distance resolution of the accumulator in pixels.
-                                theta=1*np.pi / 100,    # Angle resolution of the accumulator in radians.
-                                threshold=130   # Accumulator threshold parameter. Only those lines are returned that get enough votes ( >threshold ).
-                                )
-    tmp_image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
-    houghLinesImage = np.zeros_like(tmp_image)
-
-
-    if houghLines is not None:
-        houghLines = houghLines.reshape(houghLines.shape[0], houghLines.shape[2])
-        houghLinesImage = drawhoughLinesOnImage(tmp_image, houghLinesImage, houghLines)
-
-
-    return houghLines, houghLinesImage
 
 def houghLinesP(image):
     houghLines = cv2.HoughLinesP(image,
-                                 rho=1,             # Distance resolution of the accumulator in pixels.
-                                 theta=np.pi / 180,   # Angle resolution of the accumulator in radians.
-                                 threshold=100,  # Accumulator threshold parameter. Only those lines are returned that get enough votes ( >threshold ).
+                                 rho=1,  # Distance resolution of the accumulator in pixels.
+                                 theta=np.pi / 180,  # Angle resolution of the accumulator in radians.
+                                 threshold=100,
+                                 # Accumulator threshold parameter. Only those lines are returned that get enough votes ( >threshold ).
                                  minLineLength=75,  # Minimum line length. Line segments shorter than that are rejected.
-                                 maxLineGap=75      # Maximum allowed gap between points on the same line to link them.
+                                 maxLineGap=75  # Maximum allowed gap between points on the same line to link them.
                                  )
     tmp_image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
     houghLinesImage = np.zeros_like(tmp_image)
@@ -59,6 +32,7 @@ def houghLinesP(image):
             cv2.line(houghLinesImage, (l[0], l[1]), (l[2], l[3]), (255, 0, 255), 2, cv2.LINE_AA)
 
     return houghLines, houghLinesImage
+
 
 input_file = "../clips/belgium_japan.mp4"
 # input_file = "../clips/aris_aek.mp4"
@@ -72,13 +46,18 @@ while success:
     img_c = np.copy(img)
 
     img_c = aux.detect_court(img_c)
-    # img_c = aux.detect_white_pixels(img_c)
 
     img_c = cv2.cvtColor(img_c, cv2.COLOR_BGR2GRAY)
     _, img_c = cv2.threshold(img_c, 150, 200, cv2.THRESH_BINARY)
     img_c = cv2.Canny(img_c, 500, 200)
 
     lines, final_img = houghLinesP(img_c)
+
+    line_list = []
+    for l in lines:
+        x = (l[0], l[1])
+        y = (l[2], l[3])
+        line_list.append([l, get_angle(x, y)])
 
     # show dual images
     concat = np.concatenate((img, final_img), axis=1)
