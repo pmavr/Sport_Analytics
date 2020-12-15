f38e9dfff00c5596de587447484ecbd425f3a420
panagiotis <p.mavrogiannis@outlook.com>
2020-12-16 01:49:05 +0200

    initial pix2pix implementation

---
+++ pix2pix/helper.py
@@ -0,0 +1,239 @@
+import os, glob, shutil
+import numpy as np
+from numpy import asarray
+import cv2
+import scipy.io
+from numpy import savez_compressed
+import matplotlib.pyplot as plt
+import tarfile
+
+
+def read_homography_matrix(data):
+    H = np.zeros((3, 3))
+    for i in range(len(data)):
+        H[i] = np.array([float(x) for x in data[i].strip().split()])
+    return H
+
+
+def show_image(img, msg=''):
+    """
+    Displays an image. Esc char to close window
+    :param img: Image to be displayed
+    :param msg: Optional message-title for the window
+    :return:
+    """
+    cv2.imshow(msg, img)
+    while 1:
+        k = cv2.waitKey(0)
+        if k == 27:
+            break
+    cv2.destroyWindow(msg)
+
+
+def load_images(initial_path, relative_path):
+    src_list, tar_list = list(), list()
+    court_image_path = f'{initial_path}{relative_path}court_images/'
+    homographies_path = f'{initial_path}{relative_path}homography_matrices/'
+    bin_court = plt.imread(f'{initial_path}binary_court.jpg')
+
+    dataset_length = os.listdir(court_image_path).__len__()
+
+    for i in range(1, dataset_length):
+        court_img = plt.imread('{}{}.jpg'.format(court_image_path, i))
+
+        with open('{}{}.homographyMatrix'.format(homographies_path, i)) as homography_file:
+            data = homography_file.readlines()
+        matrix = read_homography_matrix(data)
+        w = court_img.shape[0]
+        h = court_img.shape[1]
+        inv_matrix = np.linalg.inv(matrix)
+        edge_map = cv2.warpPerspective(src=bin_court, M=inv_matrix, dsize=(h, w))
+
+        src_list.append(court_img)
+        tar_list.append(edge_map)
+    return [asarray(src_list), asarray(tar_list)]
+
+
+def load_real_samples(filename):
+    data = np.load(filename)
+    X1, X2 = data['arr_0'], data['arr_1']
+    X1 = (X1 - 127.5) / 127.5
+    X2 = (X2 - 127.5) / 127.5
+    return [X1, X2]
+
+
+def generate_real_samples(dataset, n_samples, patch_shape):
+    trainA, trainB = dataset
+    ix = np.random.randint(0, trainA.shape[0], n_samples)
+    X1, X2 = trainA[ix], trainB[ix]
+    y = np.ones((n_samples, patch_shape, patch_shape, 1))
+    return [X1, X2], y
+
+
+def generate_fake_samples(g_model, samples, patch_shape):
+    x = g_model.predict(samples)
+    y = np.zeros((len(x), patch_shape, patch_shape, 1))
+    return x, y
+
+
+def summarize_performance(step, g_model, dataset, n_samples=3):
+    [X_realA, X_realB], _ = generate_real_samples(dataset, n_samples, 1)
+    X_fakeB, _ = generate_fake_samples(g_model, X_realA, 1)
+    X_realA = (X_realA + 1) / 2.0
+    X_realB = (X_realB + 1) / 2.0
+    X_fakeB = (X_fakeB + 1) / 2.0
+    for i in range(n_samples):
+        plt.subplot(3, n_samples, 1 + i)
+        plt.axis('off')
+        plt.imshow(X_realA[i])
+
+    for i in range(n_samples):
+        plt.subplot(3, n_samples, 1 + n_samples + i)
+        plt.axis('off')
+        plt.imshow(X_fakeB[i])
+
+    for i in range(n_samples):
+        plt.subplot(3, n_samples, 1 + n_samples * 2 + i)
+        plt.axis('off')
+        plt.imshow(X_realB[i])
+
+    filename1 = 'plot_%06d.png' % (step + 1)
+    plt.savefig(filename1)
+    plt.close()
+
+    filename2 = 'model_%06d.h5' % (step + 1)
+    g_model.save(filename2)
+    print('>Saved: %s and %s' % (filename1, filename2))
+
+
+def train(d_model, g_model, gan_model, dataset, n_epochs=100, n_batch=1):
+    n_patch = d_model.output_shape[1]
+    trainA, trainB = dataset
+    bat_per_epo = int(len(trainA) / n_batch)
+    n_steps = bat_per_epo * n_epochs
+
+    for i in range(n_steps):
+        [X_realA, X_realB], y_real = generate_real_samples(dataset, n_batch, n_patch)
+        X_fakeB, y_fake = generate_fake_samples(g_model, X_realA, n_patch)
+        d_loss1 = d_model.train_on_batch([X_realA, X_realB], y_real)
+        d_loss2 = d_model.train_on_batch([X_realA, X_fakeB], y_fake)
+        g_loss, _, _ = gan_model.train_on_batch(X_realA, [y_real, X_realB])
+        print('>%d, d1[%.3f] d2[%.3f] g[%.3f]' % (i + 1, d_loss1, d_loss2, g_loss))
+
+        if (i + 1) % (bat_per_epo * 10) == 0:
+            summarize_performance(i, g_model, dataset)
+
+
+def plot_images(src_img, gen_img, tar_img):
+    images = np.vstack((src_img, gen_img, tar_img))
+    images = (images + 1) / 2.0
+    titles = ['Source', 'Generated', 'Expected']
+
+    for i in range(len(images)):
+        plt.subplot(1, 3, 1 + i)
+        plt.axis('off')
+        plt.imshow(images[i])
+        plt.title(titles[i])
+    plt.show()
+
+
+def get_horizontal_flip_homography(court_img, homography_orig):
+    # taken from Homayounfar et al. - 2017
+
+    # court dimensions in yards
+    field_width = 114.83
+    field_height = 74.37
+    img_width = court_img.shape[1]
+    img_height = court_img.shape[0]
+
+    homography_rev = np.array([[-1, 0, img_width], [0, 1, 0], [0, 0, 1]])
+    homography_rev_inv = homography_rev
+    homography_rev_model = np.array([[-1, 0, field_width], [0, 1, 0], [0, 0, 1]])
+
+    homography = homography_orig.dot(homography_rev_inv)
+    homography = homography_rev_model.dot(homography)
+    return homography
+
+
+def augment_soccer_dataset(initial_path, relative_path):
+    court_image_path = f'{initial_path}{relative_path}court_images/'
+    homographies_path = f'{initial_path}{relative_path}homography_matrices/'
+    grass_mat_path = f'{initial_path}{relative_path}grass_mats/'
+
+    dataset_length = os.listdir(court_image_path).__len__()
+
+    for i in range(1, dataset_length + 1):
+        court_img = cv2.imread('{}{}.jpg'.format(court_image_path, i))
+
+        mat = scipy.io.loadmat('{}{}_grass_gt.mat'.format(grass_mat_path, i))
+        grass_mask = mat.get('grass')
+
+        with open('{}{}.homographyMatrix'.format(homographies_path, i)) as homography_file:
+            data = homography_file.readlines()
+        matrix = read_homography_matrix(data)
+
+        flipped_court_img = cv2.flip(court_img, 1)
+        flipped_grass_mask = cv2.flip(grass_mask, 1)
+        flipped_mat = {'grass': flipped_grass_mask}
+        flipped_court_homography = get_horizontal_flip_homography(court_img, matrix)
+
+        flipped_image_code = i + dataset_length
+        cv2.imwrite(f'{court_image_path}{flipped_image_code}.jpg', flipped_court_img)
+        scipy.io.savemat(f'{grass_mat_path}{flipped_image_code}_grass_gt.mat', flipped_mat, do_compression=True)
+        np.savetxt(f'{homographies_path}{flipped_image_code}.homographyMatrix',
+                   flipped_court_homography, fmt='%.7e', delimiter='\t')
+
+
+def export_dataset_to_npz(initial_path, relative_path, filename):
+    [src_images, tar_images] = load_images(initial_path, relative_path)
+
+    src_images = [cv2.resize(img, (256, 256)) for img in src_images]
+    tar_images = [cv2.resize(img, (256, 256)) for img in tar_images]
+
+    src_images = asarray(src_images)
+    tar_images = asarray(tar_images)
+
+    savez_compressed(filename, src_images, tar_images)
+    print('Saved dataset: ', filename)
+
+
+def divide_dataset_to_folders(initial_path, relative_path):
+    path = f'{initial_path}{relative_path}'
+    os.mkdir(f'{path}court_images')
+    os.mkdir(f'{path}homography_matrices')
+    os.mkdir(f'{path}grass_mats')
+
+    court_image_files = glob.glob(f'{path}*.jpg')
+    homography_files = glob.glob(f'{path}*.homographyMatrix')
+    mat_files = glob.glob(f'{path}*.mat')
+
+    for file in court_image_files:
+        shutil.move(file, f'{path}court_images')
+
+    for file in homography_files:
+        shutil.move(file, f'{path}homography_matrices')
+
+    for file in mat_files:
+        shutil.move(file, f'{path}grass_mats')
+
+
+if __name__ == '__main__':
+
+    initial_path = 'dataset/'
+    dataset_tar_file = 'soccer_data.tar.gz'
+
+    my_tar = tarfile.open(f'{initial_path}{dataset_tar_file}')
+    my_tar.extractall(path=f'{initial_path}')
+    my_tar.close()
+
+    relative_train_path = 'raw/train_val/'
+    relative_test_path = 'raw/test/'
+
+    divide_dataset_to_folders(initial_path, relative_train_path)
+    divide_dataset_to_folders(initial_path, relative_test_path)
+
+    augment_soccer_dataset(initial_path, relative_train_path)
+    augment_soccer_dataset(initial_path, relative_test_path)
+
+    export_dataset_to_npz(initial_path, relative_train_path, filename='train_maps_256.npz')
+    export_dataset_to_npz(initial_path, relative_test_path, filename='test_maps_256.npz')
---
+++ pix2pix/model.py
@@ -0,0 +1,123 @@
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.initializers import RandomNormal
+from tensorflow.keras.models import Model
+from tensorflow.keras import Input
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Conv2DTranspose
+from tensorflow.keras.layers import LeakyReLU
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Concatenate
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import LeakyReLU
+
+
+def define_discriminator(image_shape=(256, 256, 3)):
+    init = RandomNormal(stddev=0.02)
+
+    in_src_image = Input(shape=image_shape)
+
+    in_target_image = Input(shape=image_shape)
+
+    merged = Concatenate()([in_src_image, in_target_image])
+
+    d = Conv2D(64, (4, 4), strides=(2, 2), padding='same', kernel_initializer=init)(merged)
+    d = LeakyReLU(alpha=0.2)(d)
+
+    d = Conv2D(128, (4, 4), strides=(2, 2), padding='same', kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+
+    d = Conv2D(256, (4, 4), strides=(2, 2), padding='same', kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+
+    d = Conv2D(512, (4, 4), strides=(2, 2), padding='same', kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+
+    d = Conv2D(512, (4, 4), padding='same', kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+
+    d = Conv2D(1, (4, 4), padding='same', kernel_initializer=init)(d)
+    patch_out = Activation('sigmoid')(d)
+
+    model = Model([in_src_image, in_target_image], patch_out)
+
+    opt = Adam(lr=0.0002, beta_1=0.5)
+    model.compile(loss='binary_crossentropy', optimizer=opt, loss_weights=[0.5])
+    return model
+
+
+def define_encoder_block(layer_in, n_filters, batchnorm=True):
+    init = RandomNormal(stddev=0.02)
+
+    g = Conv2D(n_filters, (4, 4), strides=(2, 2), padding='same', kernel_initializer=init)(layer_in)
+
+    if batchnorm:
+        g = BatchNormalization()(g, training=True)
+
+    g = LeakyReLU(alpha=0.2)(g)
+    return g
+
+
+def decoder_block(layer_in, skip_in, n_filters, dropout=True):
+    init = RandomNormal(stddev=0.02)
+    g = Conv2DTranspose(n_filters, (4, 4), strides=(2, 2), padding='same', kernel_initializer=init)(layer_in)
+    g = BatchNormalization()(g, training=True)
+
+    if dropout:
+        g = Dropout(0.5)(g, training=True)
+
+    g = Concatenate()([g, skip_in])
+
+    g = Activation('relu')(g)
+    return g
+
+
+def define_generator(image_shape=(256, 256, 3)):
+    init = RandomNormal(stddev=0.02)
+
+    in_image = Input(shape=image_shape)
+
+    e1 = define_encoder_block(layer_in=in_image, n_filters=64, batchnorm=False)
+    e2 = define_encoder_block(layer_in=e1, n_filters=128)
+    e3 = define_encoder_block(layer_in=e2, n_filters=256)
+    e4 = define_encoder_block(layer_in=e3, n_filters=512)
+    e5 = define_encoder_block(layer_in=e4, n_filters=512)
+    e6 = define_encoder_block(layer_in=e5, n_filters=512)
+    e7 = define_encoder_block(layer_in=e6, n_filters=512)
+
+    b = Conv2D(512, (4, 4), strides=(2, 2), padding='same', kernel_initializer=init)(e7)
+    b = Activation('relu')(b)
+
+    d1 = decoder_block(layer_in=b, skip_in=e7, n_filters=512)
+    d2 = decoder_block(layer_in=d1, skip_in=e6, n_filters=512)
+    d3 = decoder_block(layer_in=d2, skip_in=e5, n_filters=512)
+    d4 = decoder_block(layer_in=d3, skip_in=e4, n_filters=512, dropout=False)
+    d5 = decoder_block(layer_in=d4, skip_in=e3, n_filters=256, dropout=False)
+    d6 = decoder_block(layer_in=d5, skip_in=e2, n_filters=128, dropout=False)
+    d7 = decoder_block(layer_in=d6, skip_in=e1, n_filters=64, dropout=False)
+
+    g = Conv2DTranspose(3, (4, 4), strides=(2, 2), padding='same', kernel_initializer=init)(d7)
+    out_image = Activation('tanh')(g)
+
+    model = Model(in_image, out_image)
+    return model
+
+
+def define_gan(g_model, d_model, image_shape):
+    d_model.trainable = False
+
+    in_src = Input(shape=image_shape)
+
+    gen_out = g_model(in_src)
+
+    dis_out = d_model([in_src, gen_out])
+
+    model = Model(in_src, [dis_out, gen_out])
+
+    opt = Adam(lr=0.0002, beta_1=0.5)
+    model.compile(loss=['binary_crossentropy', 'mae'], optimizer=opt, loss_weights=[1, 100])
+    return model
